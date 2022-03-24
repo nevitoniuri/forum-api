644c87d3b4a81f38f8cd38f8931812e4fdcda4f3
Iuri Neviton <iurineviton@gmail.com>
2022-03-24 16:40:08 -0300

    autenticação

--- src/main/java/br/com/alura/forum/model/Usuario.java
+++ src/main/java/br/com/alura/forum/model/Usuario.java
@@ -1,12 +1,20 @@
 package br.com.alura.forum.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +22,9 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
@@ -40,36 +51,38 @@ public class Usuario {
 		return true;
 	}
 
-	public Long getId() {
-		return id;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	@Override
+	public String getPassword() {
+		return this.senha;
 	}
 
-	public String getNome() {
-		return nome;
+	@Override
+	public String getUsername() {
+		return this.nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
 	}
 
-	public String getEmail() {
-		return email;
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
 	}
 
-	public String getSenha() {
-		return senha;
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$C6nmeZSSCyyFVe9Z4Nfv9eA3YzcEp37H16587st1rgQuIpz/p1SmO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
